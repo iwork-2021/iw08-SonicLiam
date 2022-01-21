6c3f1a7b18702b8b8c08e52b7e41e2f2c1568276
SonicLiam <191220099@smail.nju.edu.cn>
2022-01-21 19:15:04 +0800

    finish

--- starter/SMDB/SMDB/NLPHelper.swift
+++ starter/SMDB/SMDB/NLPHelper.swift
@@ -31,6 +31,7 @@
 /// THE SOFTWARE.
 
 import NaturalLanguage
+import CoreML
 
 func getLanguage(text: String) -> NLLanguage? {
   NLLanguageRecognizer.dominantLanguage(for: text)
@@ -119,10 +120,75 @@ func predictSentiment(
 
 func getSentences(text: String) -> [String] {
   // To be replaced
-  return []
+  // return []
+    let tokenizer = NLTokenizer(unit: .sentence)
+    tokenizer.string = text
+    let sentenceRanges = tokenizer.tokens(
+        for: text.startIndex..<text.endIndex)
+    return sentenceRanges.map { String(text[$0]) }
 }
-
+let maxOutSequenceLength = 87
+let startTokenIndex = 0
+let stopTokenIndex = 1
 func spanishToEnglish(text: String) -> String? {
-  // To be replaced
-  return nil
+    guard let encoderIn = getEncoderInput(text) else {
+        return nil
+    }
+    let decoderIn = getDecoderInput(encoderInput: encoderIn)
+    let decoder = Es2EnCharDecoder16Bit()
+    var translatedText: [Character] = []
+    var doneDecoding = false
+    var decodedIndex = startTokenIndex
+    
+    while !doneDecoding {
+        decoderIn.encodedChar[decodedIndex] = 1
+        
+        let decoderOut = try! decoder.prediction(input: decoderIn)
+        decoderIn.decoder_lstm_h_in = decoderOut.decoder_lstm_h_out
+        decoderIn.decoder_lstm_c_in = decoderOut.decoder_lstm_c_out
+        decoderIn.encodedChar[decodedIndex] = 0
+        
+        decodedIndex = argmax(array: decoderOut.nextCharProbs)
+        if decodedIndex == stopTokenIndex {
+            doneDecoding = true
+        } else {
+            translatedText.append(intToEnChar[decodedIndex]!)
+        }
+        
+        if translatedText.count >= maxOutSequenceLength {
+            doneDecoding = true
+        }
+    }
+    
+    return String(translatedText)
+}
+let esCharToInt = loadCharToIntJsonMap(from: "esCharToInt")
+let intToEnChar = loadIntToCharJsonMap(from: "intToEnChar")
+func getEncoderInput(_ text: String) -> MLMultiArray?{
+    let cleanedText = text.filter { esCharToInt.keys.contains($0) }
+    if cleanedText.isEmpty {
+      return nil
+    }
+    let vocabSize = esCharToInt.count
+    let encoderIn = initMultiArray(shape: [NSNumber(value: cleanedText.count),1,
+                          NSNumber(value: vocabSize)])
+
+    for (i, c) in cleanedText.enumerated() {
+      encoderIn[i * vocabSize + esCharToInt[c]!] = 1
+    }
+
+    return encoderIn
+}
+func getDecoderInput(encoderInput: MLMultiArray) -> Es2EnCharDecoder16BitInput{
+    let encoder = Es2EnCharEncoder16Bit()
+    let encoderOut = try! encoder.prediction(
+      encodedSeq: encoderInput,
+      encoder_lstm_h_in: nil,
+      encoder_lstm_c_in: nil
+      )
+    let decoderIn = initMultiArray(shape: [NSNumber(value: intToEnChar.count)])
+    return Es2EnCharDecoder16BitInput(
+      encodedChar: decoderIn,
+      decoder_lstm_h_in: encoderOut.encoder_lstm_h_out,
+      decoder_lstm_c_in: encoderOut.encoder_lstm_c_out)
 }
